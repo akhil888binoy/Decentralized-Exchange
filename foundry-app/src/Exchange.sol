73a47052d7d57e0de15c9a4eef3d731b77fde32a
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2024-07-26 16:10:31 +0530

    add exc

--- foundry-app/src/Exchange.sol
+++ foundry-app/src/Exchange.sol
@@ -41,6 +41,78 @@ contract Exchange is ERC20 {
         _mint(msg.sender, lpTokensToMint);
 
         return lpTokensToMint;
-
     }
+
+    function removeLiquidity(uint256 amountOfLpTokens) public  returns(uint256, uint256){
+
+        require(amountOfLpTokens > 0 , "No enough Lp tokens");
+
+       uint256 ethReserveBalance = address(this).balance;
+       uint lpTokensTotalSupply = totalSupply();
+
+       uint256 ethToReturn =(ethReserveBalance * amountOfLpTokens)/lpTokensTotalSupply;
+       uint256 tokenToReturn = (getReserve() * amountOfLpTokens)/lpTokensTotalSupply;
+
+       _burn(msg.sender, amountOfLpTokens);
+       payable(msg.sender).transfer(ethToReturn);
+
+       ERC20(tokenAddress).transfer(msg.sender , tokenToReturn);
+        return (ethToReturn, tokenToReturn);
+    }
+
+function getOutputAmountFromSwap(
+    uint256 inputAmount , 
+    uint256 inputReserve,
+    uint256 outputReserve
+    ) public pure returns(uint256){
+
+        require(inputReserve > 0 && outputReserve > 0,
+        "Reserves must be greater than 0");
+
+        uint256 inputAmountWithFee= inputAmount * 99;
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denomiator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator/denomiator;
+    } 
+
+function ethToTokenSwap(uint256 minTokensToReceive) public payable{
+    uint256 tokenReserveBalance = getReserve();
+
+    uint256 tokensToReceive = getOutputAmountFromSwap(
+        msg.value,
+        address(this).balance-msg.value,
+        tokenReserveBalance
+    );
+    require(tokensToReceive >= minTokensToReceive,
+    "Tokens received are less than minimum tokens expected");
+
+    ERC20(tokenAddress).transfer(msg.sender, tokensToReceive);  
+}
+
+
+function tokenToEthSwap(
+    uint256 tokensToSwap,
+    uint256 minEthToReceive
+) public {
+
+    uint256 tokenReserveBalance = getReserve();
+    uint256 ethToReceive = getOutputAmountFromSwap(
+        tokensToSwap,
+        tokenReserveBalance,
+        address(this).balance
+    );
+    require(
+        ethToReceive >= minEthToReceive,
+        "ETH received is less than minimum Eth expected"
+    );
+
+    ERC20(tokenAddress).transferFrom(
+        msg.sender,
+        address(this),
+        tokensToSwap
+    );
+    payable(msg.sender).transfer(ethToReceive);
+  }
+
 }
